c36c6fb83ec5fb7136a408af27cd4b213975a1ef
plitke0899 <165337105+plitke0899@users.noreply.github.com>
2024-03-28 20:09:34 -0400

    update image aspect ratio

--- scripts.js
+++ scripts.js
@@ -3,15 +3,19 @@ $('.card').on('click', function () {
 
     $('#column-right').empty()
 
-if (artist === 'nirvana') {
-    $('#column-right').css('background-image', 'url("https://people.com/thmb/FM0c0-njIY8t6hF8V4YmbW8b-o0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/nirvana-660-0269944e1daa401199afb0b9b4a8706e.jpg")')
-}
+    if (artist === 'nirvana') {
+        $('#column-right').css('background-image', 'url("https://people.com/thmb/FM0c0-njIY8t6hF8V4YmbW8b-o0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/nirvana-660-0269944e1daa401199afb0b9b4a8706e.jpg")')
+    }
 
     if (artist === 'blink-182') {
-    $('#column-right').css('background-image', 'url("https://upload.wikimedia.org/wikipedia/en/2/21/Blink-182_-_Dude_Ranch_cover.jpg")')
-}
+        $('#column-right').css('background-image', 'url("https://upload.wikimedia.org/wikipedia/en/2/21/Blink-182_-_Dude_Ranch_cover.jpg")')
+    }
 
     if (artist === 'radiohead') {
-    $('#column-right').css('background-image', 'url("https://upload.wikimedia.org/wikipedia/en/b/ba/Radioheadokcomputer.png")')
-}
+        $('#column-right').css('background-image', 'url("https://upload.wikimedia.org/wikipedia/en/b/ba/Radioheadokcomputer.png")')
+    }
+
+    if (artist === 'The 1975') {
+        $('#column-right').css('background-image', 'url("https://en.wikipedia.org/wiki/Being_Funny_in_a_Foreign_Language#/media/File:The_1975_-_Being_Funny_in_a_Foreign_Language.png")')
+    }
 })
